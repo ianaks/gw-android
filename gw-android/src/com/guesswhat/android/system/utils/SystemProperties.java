edc100117e1c178bc7d5f5e6c1f6ddd35b4494e0
iuri <iuri@fake.com>
2015-05-18 18:00:10 +0300

    - SERVER PROPERTIES

--- gw-android/src/com/guesswhat/android/system/utils/SystemProperties.java
+++ gw-android/src/com/guesswhat/android/system/utils/SystemProperties.java
@@ -6,4 +6,8 @@ public class SystemProperties {
 	public static int QUESTION_TIMER = 30000; // milliseconds
 	public static int QUESTIONS_COUNT = 10;
 	
+	public static String SERVER_URL = "1-dot-guesswhatbackend.appspot.com";
+	public static String AUTH_LOGIN = "dsgvnlsgne384ut84jkl4jgker0tgu439tugierjg84ut8934ut";
+	public static String AUTH_PASSWORD = "hfwuehr74ytuierhierjhjhbdf8g98h4j3hjkthr89gy894";
+	
 }
