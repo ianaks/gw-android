fcedc0ea88f0e0d75eb61f00ed53e41fe1ba03d6
Iana <Iana@mytoshiba>
2015-05-25 23:12:28 +0300

    - fixed async task proxy invocation

--- gw-android/src/com/guesswhat/android/service/cfg/AsyncTaskServiceRegister.java
+++ gw-android/src/com/guesswhat/android/service/cfg/AsyncTaskServiceRegister.java
@@ -7,19 +7,23 @@ import java.lang.reflect.Proxy;
 import android.os.AsyncTask;
 
 public class AsyncTaskServiceRegister {
-	
-	private static InvocationHandler invocationHandler = new ServiceInvocationHandler();
 
 	@SuppressWarnings("unchecked")
 	public static <T> T proxify(T service) {
 		Class<? extends Object> clazz = service.getClass();
-		return (T) Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), invocationHandler);
+		return (T) Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(), new ServiceInvocationHandler(service));
 	}
 	
 	private static class ServiceInvocationHandler implements InvocationHandler {
 
+		private Object service = null;
+		
+		public ServiceInvocationHandler(Object service) {
+			this.service = service;
+		}
+
 		@Override
-		public Object invoke(final Object service, final Method method, final Object[] params)
+		public Object invoke(Object proxy, final Method method, final Object[] params)
 				throws Throwable {
 			
 			AsyncTask<Object, Object, Object> asyncTask = new AsyncTask<Object, Object, Object>() {
@@ -37,7 +41,7 @@ public class AsyncTaskServiceRegister {
 				
 			};
 			
-			return asyncTask.execute(service);
+			return asyncTask.execute(params);
 		}
 		
 	}
